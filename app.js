cef2f4a90f4353d38ab2231bf14f65bb46921c81
pietroiusti <giulio.pietroiusti@gmail.com>
2022-08-29 14:01:29 +0200

    Fix cors issues.
    Fix cors issues.
    This has been useful:
    https://stackoverflow.com/questions/25727306/request-header-field-access-control-allow-headers-is-not-allowed-by-access-contr

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 let dataFilePath = './data.json';
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+
   //Add error listener
   req.on('error', (err) => {
     console.error(err);
@@ -17,6 +20,7 @@ const server = http.createServer((req, res) => {
   });
 
   if (req.method === 'GET') {
+    console.log('GET');
     if (req.url === '/') {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
@@ -25,15 +29,29 @@ const server = http.createServer((req, res) => {
       var obj = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
       let response = obj;
       res.end(JSON.stringify(response));
+    } else if (req.url === '/rooms') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST');
+
+      var obj = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
+      let response = obj.rooms;
+      res.end(JSON.stringify(response));
     } else {
       res.statusCode = 404;
       res.end();
     }
   } else if (req.method === 'PUT') {
+    console.log('PUT');
     // Receive updated room
     //
     // USE room id to replace old room
     //
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
     const chunks = [];
     req.on('data', chunk => chunks.push(chunk));
     req.on('end', () => {
@@ -43,14 +61,24 @@ const server = http.createServer((req, res) => {
       let oldData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
       //let oldRoom = oldData.rooms.find( r => r.id === id );      
 
+      console.log(oldData);
+
       let updatedData = oldData;
       updatedData.rooms[updatedRoom.id - 1] = updatedRoom; //<< we rely on order
       updatedData = JSON.stringify(updatedData);
       
       // write file
       fs.writeFileSync(dataFilePath, updatedData);
-      res.end('done\n');
+      res.end();
     })
+  } else if (req.method === 'OPTIONS') {
+    console.log('OPTIONS');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST');
+    //https://stackoverflow.com/questions/25727306/request-header-field-access-control-allow-headers-is-not-allowed-by-access-contr
+    res.end();
   } else {
     res.statusCode = 404;
     res.end();
